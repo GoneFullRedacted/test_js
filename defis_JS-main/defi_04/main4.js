9983e7c3f0432da3a38a08075d09df3b8fcd535c
GoneFullRedacted <s.dauvergne58@gmail.com>
2025-05-13 16:51:44 +0200

    ;j;;

--- defis_JS-main/defi_04/main4.js
+++ defis_JS-main/defi_04/main4.js
@@ -1,8 +1,8 @@
-let pw  = document.getElementById('password').value
-let pwcheck = document.getElementById('check-password').value
+let pw  = document.getElementById('password')
+let pwcheck = document.getElementById('check-password')
 let warn = document.getElementById('message')
 let valid = document.getElementById('btn')
 
 valid.addEventListener('click', () => {
-    if 
+    if (pw.value === pwcheck.value)
 })
